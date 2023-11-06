2ae1678ca2d818ddb4bda1cd8defc9f4281efa16
LoisClinton <loisclinton@hotmail.co.uk>
2023-11-06 20:37:59 +0000

    completed part 2 and part 3

--- src/app.js
+++ src/app.js
@@ -10,4 +10,54 @@ app.get("/restaurants", async (request, resolve) => {
   resolve.json(restaurants);
 });
 
+app.get("/restaurants/:id", async (request, resolve) => {
+  const id = request.params.id;
+  const restaurants = await Restaurant.findByPk(id);
+  // Send restaurants as converted to a JSON string .
+  resolve.json(restaurants);
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+// request objects’ body using request.body
+
+app.post("/restaurants", async (request, resolve) => {
+  await Restaurant.create({
+    name: request.body.name,
+    location: request.body.location,
+    cuisine: request.body.cuisine,
+  });
+
+  const restaurants = await Restaurant.findAll();
+  resolve.json(restaurants);
+});
+
+app.put("/restaurants/:id", async (request, resolve) => {
+  await Restaurant.update(
+    {
+      name: request.body.name,
+      location: request.body.location,
+      cuisine: request.body.cuisine,
+    },
+    {
+      where: {
+        id: request.params.id,
+      },
+    }
+  );
+  const thisRestaurant = await Restaurant.findByPk(request.params.id);
+  resolve.json(thisRestaurant);
+});
+
+app.delete("/restaurants/:id", async (request, resolve) => {
+  await Restaurant.destroy({
+    where: {
+      id: request.params.id,
+    },
+  });
+
+  resolve.send("Item deleted");
+});
+
 module.exports = app;
